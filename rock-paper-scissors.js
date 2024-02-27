19b007e28b6fe45e98a58836a0049a27e092cca1
devsandroses <145285251+devsandroses@users.noreply.github.com>
2024-02-27 07:49:44 -0800

    reworked code for even listeners, cleaned up css

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -45,6 +45,10 @@ function playRound(playerSelection, computerSelection){
         return "Player wins!"
     }
 }
+// Add an event listener to the buttons that call your playRound function with the correct playerSelection
+//every time a button is clicked. (you can keep the console.log s for this step)
+//Add a div for displaying results and change all of your console.logs into DOM methods.
+//Display the running score, and announce a winner of the game once one player reaches 5 points.
 // Play 5 rounds using loop
 function playGame(n){
     for (let i=1; i<=n; i++){
